4ff65ae6e56754693e070a26009f5bf1b907704b
kadkins-sysdig <91559003+kadkins-sysdig@users.noreply.github.com>
2022-11-21 13:42:33 -0600

    Update download_report_by_schedule_name.sh
    fixed path to python script

--- secure-api-reporting-v2/python/download_report_by_schedule_name.sh
+++ secure-api-reporting-v2/python/download_report_by_schedule_name.sh
@@ -1,5 +1,5 @@
 # A sample unix shell script
-python3 .\download_report_by_schedule_name.py \
+python3 ./download_report_by_schedule_name.py \
   --secure_url_authority secure.sysdig.com \
   --api_token "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" \
   --schedule_name "A Test Report" \
