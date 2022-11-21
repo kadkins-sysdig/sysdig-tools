713490dd67a04b24efd471ecab19a20612bdc29f
kadkins-sysdig <91559003+kadkins-sysdig@users.noreply.github.com>
2022-11-21 10:18:24 -0600

    Create download_report_by_schedule_name.ps1

---
+++ secure-api-reporting-v2/python/download_report_by_schedule_name.ps1
@@ -0,0 +1,6 @@
+# A sample powershell script
+python3 .\download_report_by_schedule_name.py `
+  --secure_url_authority secure.sysdig.com `
+  --api_token "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" `
+  --schedule_name "A Test Report" `
+  --decompress
