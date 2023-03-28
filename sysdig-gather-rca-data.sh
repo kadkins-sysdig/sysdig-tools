f4904659e7c0f5a0c07fb5f24102f96454f861fe
kadkins-sysdig <91559003+kadkins-sysdig@users.noreply.github.com>
2023-03-28 14:05:45 -0500

    switch to sysdig-agent namespace

--- sysdig-gather-rca-data.sh
+++ sysdig-gather-rca-data.sh
@@ -2,7 +2,8 @@
 set -x #echo on
 
 # Set to the namespace you are running Sysdig in
-NAMESPACE=kube-system
+#NAMESPACE=kube-system
+NAMESPACE=sysdig-agent
 
 SFX=`date +"%Y-%m-%d_%H-%M-%S"`
 OUTPUT_DIR=sysdig-${SFX}
