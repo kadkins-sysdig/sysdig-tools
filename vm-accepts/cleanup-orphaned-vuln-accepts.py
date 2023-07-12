f86d5f843f2c40a687b7814094a0b6c2867f089b
kadkins-sysdig <91559003+kadkins-sysdig@users.noreply.github.com>
2023-07-12 11:52:50 -0500

    Update cleanup-orphaned-vuln-accepts.py
    Added delete and save options.

--- vm-accepts/cleanup-orphaned-vuln-accepts.py
+++ vm-accepts/cleanup-orphaned-vuln-accepts.py
@@ -1,8 +1,4 @@
 """
-  WORK IN PROGRESS:
-  TODO - add an argument for delete yes/no
-  TODO - add an argument to save off orphaned accepts to a file
-
   This python script will list and/or remove orphaned vulnerability management
   accepts. An accept for an image that is no longer running will be considered
   an orphan.
@@ -11,8 +7,8 @@
   Date July 11th, 2023
 
   Currently Handles:
-     accept.context.contextType: imageName (Image CVE)
-     accept.context.contextType: "" (Global Image)
+     accept.context_type.context.contextType == "imageName" (Image CVE)
+     accept.entity_type == "imageName" (Global Image)
 
   Currently Does NOT handle:
      accept.context.contextType: imageAssetToken
@@ -21,17 +17,14 @@
      accept.context.contextType: packageName
 """
 
-import argparse
 import logging
-import sys
 import urllib3
-import json
-import urllib.parse
+import sys
+import argparse
 from datetime import datetime
-import math
-import csv
-import platform
 import time
+import json
+import os
 
 # Setup logger
 LOG = logging.getLogger(__name__)
@@ -70,6 +63,19 @@ def _parse_args():
         action="store",
         help="Sysdig Secure API Token",
     )
+    parser.add_argument(
+        "--output_file",
+        required=False,
+        type=str,
+        action="store",
+        help="File to save orphans to.",
+    )
+    parser.add_argument(
+        "--delete",
+        required=False,
+        action="store_true",
+        help="Orphans will be deleted",
+    )
     return parser.parse_args()
 
 def main():
@@ -82,6 +88,12 @@ def main():
         args = _parse_args()
         secure_url_authority = args.secure_url_authority
         authentication_bearer = args.api_token
+        output_file = args.output_file
+        delete_orphans = args.delete
+
+        # Validate the output file
+        if output_file != None and os.path.isfile(output_file):
+            raise Exception(f"The output file already exists: {output_file}")
 
         # Get the timestamp of this run
         now = datetime.now()
@@ -106,25 +118,34 @@ def main():
         LOG.info(f"Found {len(all_vuln_risk_accepts)} vulnerability risk accepts.")
         
         # Get the image accepts
-        image_vuln_risk_accepts = []
+        image_vuln_risk_accept_ids = []
         if len(all_vuln_risk_accepts) > 0:
-            image_vuln_risk_accepts = _get_image_vuln_risk_accepts(all_vuln_risk_accepts)
-            LOG.info(f"Found {len(image_vuln_risk_accepts)} images with vulnerability risk accepts.")
+            image_vuln_risk_accept_ids = _get_image_vuln_risk_accepts(all_vuln_risk_accepts)
+            LOG.info(f"Found {len(image_vuln_risk_accept_ids)} images with vulnerability risk accepts.")
 
         # Identify orphaned accepts
-        orphaned_risk_accepts = []
-        if len(image_vuln_risk_accepts) > 0:
-            orphaned_risk_accepts = _find_orphaned_risk_accepts(unique_runtime_image_list, image_vuln_risk_accepts)
-            LOG.info(f"Found {len(orphaned_risk_accepts)} orphaned images with vulnerability risk accepts.")
-            orphaned_risk_accepts_count = _count_orphaned_risk_accepts(orphaned_risk_accepts)
+        image_orphaned_risk_accepts_ids = []
+        if len(image_vuln_risk_accept_ids) > 0:
+            image_orphaned_risk_accepts_ids = _find_orphaned_risk_accepts(unique_runtime_image_list, image_vuln_risk_accept_ids)
+            LOG.info(f"Found {len(image_orphaned_risk_accepts_ids)} orphaned images with vulnerability risk accepts.")
+            orphaned_risk_accepts_count = _count_orphaned_risk_accepts(image_orphaned_risk_accepts_ids)
             LOG.info(f"Found {orphaned_risk_accepts_count} orphaned vulnerability risk accepts.")
 
+        # Save orphaned accepts to a file
+        if output_file != None and len(image_orphaned_risk_accepts_ids) > 0:
+            _save_orphaned_accepts(output_file, all_vuln_risk_accepts, image_orphaned_risk_accepts_ids)
+            LOG.info(f"Saved orphaned risk accepts to: {output_file}")
+        elif output_file != None and len(image_orphaned_risk_accepts_ids) == 0:
+            LOG.info(f"There is nothing to save to: {output_file}")
+
         # Delete orphaned accepts
-#KAA        if len(orphaned_risk_accepts) > 0:
-#KAA            _delete_orphaned_risk_accepts(secure_url_authority, orphaned_risk_accepts)
-#KAA            LOG.info(f"Deleted orphaned image vulnerability risk accepts.")
-#KAA        else:
-#KAA            LOG.info(f"No orphaned image vulnerability risk accepts found.")
+        if not delete_orphans and len(image_orphaned_risk_accepts_ids) > 0:
+            LOG.info(f"Skipping delete of orphaned vulnerability risk accepts.")
+        elif delete_orphans and len(image_orphaned_risk_accepts_ids) > 0:
+            _delete_orphaned_risk_accepts(secure_url_authority, image_orphaned_risk_accepts_ids)
+            LOG.info(f"Deleted orphaned image vulnerability risk accepts.")
+        else:
+            LOG.info(f"No orphaned image vulnerability risk accepts found.")
 
         # End performance counter
         pc_end = time.perf_counter()
@@ -137,44 +158,61 @@ def main():
         LOG.critical('Request to cleanup orphaned accepts failed.')
         raise SystemExit()
 
-def _count_orphaned_risk_accepts(orphaned_risk_accepts):
+def _save_orphaned_accepts(output_file, all_vuln_risk_accepts, image_orphaned_risk_accepts_ids):
+    
+    orphaned_accept_ids = []
+    accepts_to_save = []
+   
+    for image_name in image_orphaned_risk_accepts_ids.keys():
+        for accept_def_id in image_orphaned_risk_accepts_ids[image_name]:
+            orphaned_accept_ids.append(accept_def_id)
+
+    for accept in all_vuln_risk_accepts:
+        acceptId = accept['riskAcceptanceDefinitionID']
+        if acceptId in orphaned_accept_ids:
+            accepts_to_save.append(accept)
+
+    with open(output_file, "w") as outfile:
+        json.dump(accepts_to_save, outfile, indent=2)
+
+def _count_orphaned_risk_accepts(image_orphaned_risk_accepts_ids):
 
     count = 0
 
-    for image_name in orphaned_risk_accepts.keys():
-        for accept_def_id in orphaned_risk_accepts[image_name]:
+    for image_name in image_orphaned_risk_accepts_ids.keys():
+        for accept_def_id in image_orphaned_risk_accepts_ids[image_name]:
             count += 1
 
     return count
 
-def _delete_orphaned_risk_accepts(secure_url_authority, orphaned_risk_accepts):
+def _delete_orphaned_risk_accepts(secure_url_authority, image_orphaned_risk_accepts_ids):
 
     api_path = "api/scanning/riskmanager/v2/definitions"
 
-    for image_name in orphaned_risk_accepts.keys():
-        for accept_def_id in orphaned_risk_accepts[image_name]:
+    for image_name in image_orphaned_risk_accepts_ids.keys():
+        for accept_def_id in image_orphaned_risk_accepts_ids[image_name]:
             api_url = f"https://{secure_url_authority}/{api_path}/{accept_def_id}"
             _delete_data_from_http_request(api_url)
 
     return
 
-def _find_orphaned_risk_accepts(unique_runtime_image_list, image_vuln_risk_accepts):
+def _find_orphaned_risk_accepts(unique_runtime_image_list, image_vuln_risk_accept_ids):
 
-    orphaned_risk_accepts = []
-    image_vuln_risk_accepts_copy = image_vuln_risk_accepts.copy()
+    image_orphaned_risk_accepts_ids = []
+    image_vuln_risk_accept_ids_copy = image_vuln_risk_accept_ids.copy()
 
-    images_still_running = [value for value in image_vuln_risk_accepts.keys() if value in unique_runtime_image_list]
+    images_still_running = [value for value in image_vuln_risk_accept_ids.keys() if value in unique_runtime_image_list]
 
     for image in images_still_running:
-        del image_vuln_risk_accepts_copy[image]
+        del image_vuln_risk_accept_ids_copy[image]
 
-    orphaned_risk_accepts = image_vuln_risk_accepts_copy
+    image_orphaned_risk_accepts_ids = image_vuln_risk_accept_ids_copy
 
-    return orphaned_risk_accepts
+    return image_orphaned_risk_accepts_ids
 
 def _get_image_vuln_risk_accepts(all_vuln_risk_accepts):
 
-    image_vuln_risk_accepts = {}
+    image_vuln_risk_accept_ids = {}
 
     for accept in all_vuln_risk_accepts:
         entity_type = accept['entityType']
@@ -188,27 +226,25 @@ def _get_image_vuln_risk_accepts(all_vuln_risk_accepts):
                 accept_def_id = accept['riskAcceptanceDefinitionID']
                 image_name = accept['context'][0]['contextValue']
 
-                if image_name not in image_vuln_risk_accepts.keys():
-                    image_vuln_risk_accepts[image_name] = []
+                if image_name not in image_vuln_risk_accept_ids.keys():
+                    image_vuln_risk_accept_ids[image_name] = []
 
-                image_vuln_risk_accepts[image_name].append(accept_def_id)
-
-            #end if
+                image_vuln_risk_accept_ids[image_name].append(accept_def_id)
 
         # Global Image Accepts
-        elif entity_type == "vulnerability" and len(accept['context']) == 0:
+        elif entity_type == "imageName":
 
             accept_def_id = accept['riskAcceptanceDefinitionID']
             image_name = accept['entityValue']
 
-            if image_name not in image_vuln_risk_accepts.keys():
-                image_vuln_risk_accepts[image_name] = []
+            if image_name not in image_vuln_risk_accept_ids.keys():
+                image_vuln_risk_accept_ids[image_name] = []
 
-            image_vuln_risk_accepts[image_name].append(accept_def_id)
+            image_vuln_risk_accept_ids[image_name].append(accept_def_id)
 
     #end for
 
-    return image_vuln_risk_accepts
+    return image_vuln_risk_accept_ids
 
 
 def _get_all_vuln_risk_accepts(secure_url_authority):
