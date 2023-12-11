e7e13921b97a1b16bbff72f2f55bdce940b2382b
kadkins-sysdig <91559003+kadkins-sysdig@users.noreply.github.com>
2023-12-11 09:54:52 -0600

    Update gather-runtime-scanner-logs.sh

--- gather-runtime-scanner-logs.sh
+++ gather-runtime-scanner-logs.sh
@@ -5,21 +5,10 @@ set -x #echo on
 NAMESPACE=kube-system
 
 SFX=`date +"%Y-%m-%d_%H-%M-%S"`
-OUTPUT_DIR=sysdig-${SFX}
+OUTPUT_DIR=sysdig-rs-scan-logs-${SFX}
 
 mkdir ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
-
-#SA_PODS=`kubectl get pods -n ${NAMESPACE} -o json | jq -r '.items[].metadata | select(.ownerReferences[].name=="sysdig-agent") | .name'`
-#for pod in $SA_PODS
-#do
-#  mkdir sa-${pod}
-#  cd sa-${pod}
-#  #kubectl -n ${NAMESPACE} -c sysdig cp ${pod}:/opt/draios/logs .
-#  kubectl -n ${NAMESPACE} -c sysdig cp ${pod}:/opt/draios/logs/draios.log draios.log
-#  kubectl -n ${NAMESPACE} describe pod ${pod} > ${pod}_describe.txt
-#  cd ..
-#done
 
 SANA_PODS=`kubectl get pods -n ${NAMESPACE} -o json | jq -r '.items[].metadata | select(.ownerReferences[].name=="sysdig-agent-node-analyzer") | .name'`
 for pod in $SANA_PODS
@@ -30,17 +19,6 @@ do
   kubectl -n ${NAMESPACE} describe pod ${pod} > ${pod}_describe.txt
   cd ..
 done
-
-#kubectl -n ${NAMESPACE} get ds sysdig-agent -o yaml > sa-ds.yaml
-#kubectl -n ${NAMESPACE} get cm sysdig-agent -o yaml > sa-cm.yaml
-#kubectl -n ${NAMESPACE} get ds sysdig-agent-node-analyzer -o yaml > sana-ds.yaml
-#kubectl -n ${NAMESPACE} get cm sysdig-agent-runtime-scanner -o yaml > sana-cm.yaml
-#kubectl get pods -n ${NAMESPACE} -o wide > sysdig-running-pods.txt
-#kubectl get ds -n ${NAMESPACE} -o wide > sysdig-running-ds.txt
-#kubectl get nodes -o json | jq -r '.items[].status.images[] | .sizeBytes' | sort -nr | head -1 > largest-image-size.txt
-#kubectl cluster-info dump | grep -i clusterip > clusterip.txt
-#kubectl describe nodes > nodes-describe.txt
-#kubectl get events -A --sort-by=.metadata.creationTimestamp > all-events.txt 
 
 cd ..
 tar -czvf ${OUTPUT_DIR}.tar.gz ./${OUTPUT_DIR}
